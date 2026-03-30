2e80d26b9c4f8586d2815f5698fe5ce5dcc553f1
Parham Majd <parham@parhammajd.co.uk>
2026-03-30 23:21:29 +0100

    feat: add initial asset files and main application structure

---
+++ OpenClaw-Mac/OpenClaw_MacApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenClaw_MacApp.swift
+//  OpenClaw-Mac
+//
+//  Created by Parham on 30/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct OpenClaw_MacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
